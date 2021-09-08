87790d7064fd474542e33e79796a72877cbf211e
Thiago Guerrero <tgbalera@hotmail.com>
2021-09-08 20:38:42 -0300

    refactoring
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     @Query("SELECT seller, SUM(obj.amount) " +
